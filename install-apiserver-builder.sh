e129b2503c72193c0e34b68c1978399ea4765467
Chun Cheng Lin <jinsenglin@users.noreply.github.com>
2018-05-26 17:06:08 +0800

    Update install-apiserver-builder.sh

--- install-apiserver-builder.sh
+++ install-apiserver-builder.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 wget https://github.com/kubernetes-incubator/apiserver-builder/releases/download/v1.9-alpha.4/apiserver-builder-v1.9-alpha.4-darwin-amd64.tar.gz
 
 tar -zxf apiserver-builder-v1.9-alpha.4-darwin-amd64.tar.gz && rm apiserver-builder-v1.9-alpha.4-darwin-amd64.tar.gz
